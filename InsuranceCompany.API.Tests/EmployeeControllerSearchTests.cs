69c8ab7fb4b16c7c47e1196cbdcaf2317a508ef2
ssmlnsk <ssmlnsk@mail.ru>
2021-01-25 12:50:12 +0300

    first commit

---
+++ InsuranceCompany.API.Tests/EmployeeControllerSearchTests.cs
@@ -0,0 +1,15 @@
+using System;
+using Xunit;
+using InsuranceCompany.API.Controllers;
+
+namespace InsuranceCompany.API.Tests
+{
+    public class EmployeeControllerSearchTests
+    {
+        [Fact]
+        public void ItExists()
+        {
+            var controller = new EmployeeController();
+        }
+    }
+}
---
+++ InsuranceCompany.API/Controllers/EmployeeController.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+
+namespace InsuranceCompany.API.Controllers
+{
+     [Route("api/[controller]")]
+     public class EmployeeController : Controller
+     {
+      }
+}
